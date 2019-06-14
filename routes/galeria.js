6bac68a7410c76622e1aa5b0aaf12332298554b2
MurasakiRyuu <joseangel_94@hotmail.com>
2019-06-14 11:44:36 -0500

    Subida por bateria baja xdxdxd

---
+++ routes/galeria.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/mazda', function (req, res, next){
+//	rest.send("galeria mazda");
+
+//	var data=[{}]; // arreglo de onjetos JSON
+
+	var data={autos:[]}; 
+	var auto={};
+
+	auto=null;
+	auto={};
+
+	auto.nombre="Mazda 3";
+	auto.foto="https://media.metrolatam.com/2017/06/07/screen-shot-20170607-at-6.11.52-pm-600x400.jpg";
+	data.autos.push(auto);
+
+	auto=null;
+	auto={};
+
+	auto.nombre="Mazda 6";
+	auto.foto="https://mazkomazda.com/wp-content/uploads/2018/11/car-01-deep-crystal-blue-mica.png";
+	data.autos.push(auto);
+
+	auto=null;
+	auto={};
+
+	auto.nombre="Mazda CX-3";
+	auto.foto="https://static.cargurus.com/images/site/2018/11/02/00/41/2019_mazda_cx-3_grand_touring_fwd-pic-4524241946458914224-640x480.jpeg";
+	data.autos.push(auto);
+
+	console.log(data);	
+
+	res.render("./galeria/mazda", data);
+});
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AutosApp' });
+});
+
+router.get('/mazda', function(req, res, next){
+	var infoMazda={};
+	infoMazda.pais='Japón';
+	infoMazda.sectorVentas='Comercial';
+	infoMazda.fundacion=1929;
+	infoMazda.logo="https://www.mazda.mx/siteassets/mazda-mx/logos-new-mazda/mazda-logo-desktop-2.png";
+	res.render('mazda', infoMazda);
+
+});
+
+router.get('/nissan', function(req, res, next){
+
+	var infoNissan={};
+	infoNissan.pais='Japón';
+	infoNissan.sectorVentas='Comercial';
+	infoNissan.fundacion=1933;
+	infoNissan.logo="https://png.pngtree.com/element_pic/17/09/03/be7a431f5d461d9fff00ed99ea170e18.jpg";
+	res.render('nissan', infoNissan);
 });
 
 module.exports = router;
